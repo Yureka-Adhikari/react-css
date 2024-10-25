56c673d72ce12e24d025330e51a202b2934f9e88
Yureka Adhikari <adhikariyureka@gmail.com>
2024-10-25 18:59:38 +0545

    YAYAYAYYYSLLAAYAYY

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+class MyStyling extends React.Component {
+  render() {
+    // const myheaderstyle = {
+    //   color: "darkblue",
+    //   backgroundColor: "skyblue",
+    //   fontFamily: "Arial",
+    //   padding: "5px",
+    // };
+    // const mystyle = {
+    //   color: "lavender",
+    //   backgroundColor: "purple",
+    //   fontFamily: "Times New Roman",
+    //   padding: "10px",
+    // };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+    return (
+      <div>
+        {/* <h1 style={myheaderstyle}> HI! </h1>
+        <p style={mystyle}> How Are You? </p> */}
+        <h1 className="myheaderstyle"> HI! </h1>
+        <p className="mystyle"> How Are You? </p>
+      </div>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <MyStyling />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
